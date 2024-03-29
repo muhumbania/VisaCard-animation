1a5cec8e61cc7959b0f893b40a48c9cdb2284c5b
muhumbania <franklinmuhumbania@gmail.com>
2024-03-30 01:22:36 +0300

    Show text dynamic

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const button = document.querySelector('.see');
 const card = document.querySelector('.card');
+const showText = document.querySelector('.show-text');
 
 button.addEventListener('click', function(){
     card.style.transform = "rotateY(180deg)";
     setTimeout(function(){
-        document.querySelector('.visa').style.display = "none";
+        document.querySelector('.hidden-visa').style.display = "none";
+        document.querySelector('.showed-visa').style.display = "inline";
+        showText.innerHTML = "Hide details";
     }, 500);
 
 });
