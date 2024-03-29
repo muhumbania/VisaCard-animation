6291a74df86622f331eef641c9b5b7d6124d86c8
muhumbania <franklinmuhumbania@gmail.com>
2024-03-30 01:40:35 +0300

    Reversible feature added

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const button = document.querySelector('.see');
 const card = document.querySelector('.card');
 const showText = document.querySelector('.show-text');
+let position = false;
 
 button.addEventListener('click', function(){
     card.style.transform = "rotateY(180deg)";
-    setTimeout(function(){
-        document.querySelector('.hidden-visa').style.display = "none";
-        document.querySelector('.showed-visa').style.display = "inline";
-        showText.innerHTML = "Hide details";
-    }, 500);
+    position = !position;
 
+    if(position){
+        setTimeout(function(){
+            card.style.transform = "rotateY(180deg)";
+            document.querySelector('.hidden-visa').style.display = "none";
+            document.querySelector('.showed-visa').style.display = "inline";
+            showText.innerHTML = "Hide details";
+        }, 500);
+    }else{
+        setTimeout(function(){
+            card.style.transform = "rotateY(0deg)";
+            document.querySelector('.hidden-visa').style.display = "inline";
+            document.querySelector('.showed-visa').style.display = "none";
+            showText.innerHTML = "Show details";
+        }, 500);
+    }
 });
+
+
